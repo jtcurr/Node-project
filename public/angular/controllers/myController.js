0b3992dfb1d7349bb524bfde64925f3bd13de954
John Currie <johntcurrie@gmail.com>
2017-08-20 19:53:45 -0700

    Removed set timeout from chat controller

--- public/angular/controllers/myController.js
+++ public/angular/controllers/myController.js
@@ -1,6 +1,3 @@
 angularApp.controller('myController', ['$timeout', '$scope', function($timeout, $scope) {
 	$scope.mootName = 'Gil';
-	$timeout(function() {
-		$scope.mootName = 'Bacos';
-	}, 5000)
 }])
